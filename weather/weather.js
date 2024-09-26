44923a3d77dcad0cde9e0e812e64523c9d686274
sophie8910 <sli11@alaska.edu>
2024-09-25 22:03:06 -0600

    update basemap

--- weather/weather.js
+++ weather/weather.js
@@ -1,6 +1,6 @@
 var map = L.map('weathermap').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var basemap = L.tileLayer(basemapUrl).addTo(map);
+var basemap =  L.tileLayer(basemapUrl, {attribution: '&copy; <a href="http://' + 'www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
 
 
 //add the national precipitation radar layer
