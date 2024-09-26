abce6ebcd50c9a3f30e745e6c39fdecb87167752
sophie8910 <sli11@alaska.edu>
2024-09-25 21:51:27 -0600

    add radar layer

--- weather/weather.js
+++ weather/weather.js
@@ -1,3 +1,13 @@
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
+
+
+//add the national precipitation radar layer
+var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+var radarDisplayOptions = {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true
+};
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
